b00b70526046007a2502dd4105aae2ee352625a3
kosianodanngoo <80571742+kosianodanngoo@users.noreply.github.com>
2026-02-13 21:23:00 +0900

    ビームを狙いやすく

--- src/main/java/io/github/kosianodangoo/trialmonolith/common/item/BeamSummonerItem.java
+++ src/main/java/io/github/kosianodangoo/trialmonolith/common/item/BeamSummonerItem.java
@@ -1,18 +1,28 @@
 package io.github.kosianodangoo.trialmonolith.common.item;
 
 import io.github.kosianodangoo.trialmonolith.client.item.BeamSummonerItemRenderer;
+import io.github.kosianodangoo.trialmonolith.common.entity.AbstractDelayedTraceableEntity;
 import io.github.kosianodangoo.trialmonolith.common.entity.HugeBeamEntity;
 import io.github.kosianodangoo.trialmonolith.common.entity.SmallBeamEntity;
 import io.github.kosianodangoo.trialmonolith.common.init.TrialMonolithEntities;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.commands.arguments.EntityAnchorArgument;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
@@ -59,6 +69,20 @@ public class BeamSummonerItem extends Item {
     }
 
     public void shootSmallBeam(@NotNull Level pLevel, @NotNull LivingEntity pLiving) {
+        Vec3 from = pLiving.getEyePosition();
+        Vec3 to = from.add(pLiving.getViewVector(0).multiply(128, 128, 128));
+        ClipContext rayTraceContext = new ClipContext(from, to,
+                ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, pLiving);
+        HitResult hitResult = pLevel.clip(rayTraceContext);
+        Vec3 target = hitResult.getLocation();
+        EntityHitResult entityHitResult = ProjectileUtil.getEntityHitResult(pLevel, pLiving, from, to, new AABB(from, to), (entity ->
+                !(entity instanceof AbstractDelayedTraceableEntity traceable && pLiving == traceable.getOwner()) &&
+                !(entity instanceof Projectile projectile && pLiving == projectile.getOwner()) &&
+                !(entity instanceof ItemEntity)));
+        if (entityHitResult != null){
+            target = entityHitResult.getLocation().add(0,entityHitResult.getEntity().getBbHeight()/2,0);
+        }
+
         SmallBeamEntity smallBeam = new SmallBeamEntity(TrialMonolithEntities.SMALL_BEAM.get(), pLevel);
         smallBeam.setOwner(pLiving);
         RandomSource randomSource = pLevel.getRandom();
@@ -66,8 +90,7 @@ public class BeamSummonerItem extends Item {
                 pLiving.getY() + pLiving.getBbHeight() + 1.5 + randomSource.nextDouble(),
                 pLiving.getZ() + randomSource.nextDouble() - 0.5);
 
-        smallBeam.setXRot(pLiving.getXRot());
-        smallBeam.setYRot(pLiving.getYRot());
+        smallBeam.lookAt(EntityAnchorArgument.Anchor.EYES, target);
 
         pLevel.addFreshEntity(smallBeam);
     }
