9a780cf89b0d171687ddc56a71dc19fc3c278bf3
kosianodanngoo <80571742+kosianodanngoo@users.noreply.github.com>
2026-02-21 17:12:47 +0900

    tickDeath処理追加

--- src/main/java/io/github/kosianodangoo/trialmonolith/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/kosianodangoo/trialmonolith/mixin/LivingEntityMixin.java
@@ -3,17 +3,22 @@ package io.github.kosianodangoo.trialmonolith.mixin;
 import io.github.kosianodangoo.trialmonolith.TrialMonolithConfig;
 import io.github.kosianodangoo.trialmonolith.common.entity.invadermonolith.InvaderMonolithEntity;
 import io.github.kosianodangoo.trialmonolith.common.entity.trialmonolith.TrialMonolithEntity;
+import io.github.kosianodangoo.trialmonolith.common.helper.EntityHelper;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = LivingEntity.class, priority = Integer.MAX_VALUE)
 public abstract class LivingEntityMixin extends Entity {
+
+    @Shadow public abstract boolean isDeadOrDying();
 
     protected LivingEntityMixin(EntityType<? extends LivingEntity> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
@@ -25,6 +30,13 @@ public abstract class LivingEntityMixin extends Entity {
             cir.setReturnValue(TrialMonolithConfig.trialMonolithHealth);
         } else if ((Object) this instanceof InvaderMonolithEntity) {
             cir.setReturnValue(Float.POSITIVE_INFINITY);
+        }
+    }
+
+    @Inject(method = "tickDeath", at = @At("HEAD"), cancellable = true)
+    public void tickDeathMixin(CallbackInfo ci) {
+        if (EntityHelper.isSoulProtected(this) && !EntityHelper.shouldBypassProtection(this) && !this.isDeadOrDying()) {
+            ci.cancel();
         }
     }
 }
