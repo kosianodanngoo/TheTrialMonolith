02139b0bdc00c558520af939aef0e7c86c9aa5a7
kosianodanngoo <80571742+kosianodanngoo@users.noreply.github.com>
2026-02-26 20:11:54 +0900

    Swapを活用し、メソッド数削減

--- src/main/java/io/github/kosianodangoo/trialmonolith/transformer/method/EntityMethods.java
+++ src/main/java/io/github/kosianodangoo/trialmonolith/transformer/method/EntityMethods.java
@@ -23,10 +23,6 @@ public class EntityMethods {
         return 20;
     }
 
-    public static float getHealth(LivingEntity livingEntity, float health) {
-        return getHealth(health, livingEntity);
-    }
-
     public static float getHealth(float health, LivingEntity livingEntity) {
         if (EntityHelper.isSoulProtected(livingEntity)) {
             return Math.max(livingEntity.getMaxHealth(), 1);
@@ -41,10 +37,6 @@ public class EntityMethods {
         return !EntityHelper.isSoulProtected(entity) && EntityHelper.getSoulDamage(entity) >= 1;
     }
 
-    public static boolean isDeadOrDying(LivingEntity livingEntity, boolean deadOrDying) {
-        return isDeadOrDying(deadOrDying, livingEntity);
-    }
-
     public static boolean isDeadOrDying(boolean deadOrDying, LivingEntity livingEntity) {
         return (!EntityHelper.isSoulProtected(livingEntity)) && (deadOrDying || EntityHelper.getSoulDamage(livingEntity) >= 1);
     }
@@ -53,16 +45,8 @@ public class EntityMethods {
         return !replaceIsDeadOrDying(entity);
     }
 
-    public static boolean isAlive(Entity entity, boolean alive) {
-        return isAlive(alive, entity);
-    }
-
     public static boolean isAlive(boolean alive, Entity entity) {
         return EntityHelper.isSoulProtected(entity) || alive && EntityHelper.getSoulDamage(entity) < 1;
-    }
-
-    public static Entity.RemovalReason getRemovalReason(Entity entity, Entity.RemovalReason removalReason) {
-        return getRemovalReason(removalReason, entity);
     }
 
     public static Entity.RemovalReason getRemovalReason(Entity.RemovalReason removalReason, Entity entity) {
@@ -76,10 +60,6 @@ public class EntityMethods {
             return Entity.RemovalReason.KILLED;
         }
         return removalReason;
-    }
-
-    public static boolean isRemoved(Entity entity, boolean removed) {
-        return isRemoved(removed, entity);
     }
 
     public static boolean isRemoved(boolean removed, Entity entity) {
