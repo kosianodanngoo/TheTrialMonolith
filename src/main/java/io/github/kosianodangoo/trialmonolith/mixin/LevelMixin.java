861e1133fe07b381c1c459d718567028a87dffa6
kosianodanngoo <80571742+kosianodanngoo@users.noreply.github.com>
2026-03-07 15:37:23 +0900

    Tick判定を修正

--- src/main/java/io/github/kosianodangoo/trialmonolith/mixin/LevelMixin.java
+++ src/main/java/io/github/kosianodangoo/trialmonolith/mixin/LevelMixin.java
@@ -3,7 +3,6 @@ package io.github.kosianodangoo.trialmonolith.mixin;
 import io.github.kosianodangoo.trialmonolith.api.mixin.ITickTracker;
 import io.github.kosianodangoo.trialmonolith.transformer.method.EntityMethods;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +15,7 @@ import java.util.function.Consumer;
 public class LevelMixin {
     @Inject(method = "guardEntityTick", at = @At("HEAD"), cancellable = true)
     public <T extends Entity> void guardEntityTickMixin(Consumer<T> consumer, T entity, CallbackInfo ci) {
-        if (!(entity instanceof Player) && EntityMethods.shouldOverrideTick(entity) && entity instanceof ITickTracker tickTracker) {
+        if (EntityMethods.shouldOverrideTick(entity) && entity instanceof ITickTracker tickTracker) {
             if (tickTracker.the_trial_monolith$getLastTickCount() != entity.tickCount) {
                 ci.cancel();
             }
